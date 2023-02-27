790a3138ba8170b6604dfc4fa49462b16a00e023
Marcelo <ce.lo.borges@hotamil.com>
2023-02-27 20:24:37 -0300

    Adicionar arquivos de projeto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TesteBludata.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
