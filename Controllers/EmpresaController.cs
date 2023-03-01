a894d4e0f124ac23fb859b583910264e8b40ab24
Marcelo <ce.lo.borges@hotamil.com>
2023-02-28 23:10:01 -0300

    DAO Empresa e tela de listagem

---
+++ Controllers/EmpresaController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TesteBludata.Models.DAO;
+
+namespace TesteBludata.Controllers
+{
+    public class EmpresaController : Controller
+    {
+        EmpresaDAO DAO = new EmpresaDAO();
+        public IActionResult Index()
+        {
+            var list = DAO.ListEmpresa().ToList();
+            ViewBag.ListEmpresa = list;
+            return View(list);
+        }
+    }
+}
---
+++ Models/DAO/EmpresaDAO.cs
@@ -0,0 +1,154 @@
+﻿using Microsoft.AspNetCore.Authentication.OAuth.Claims;
+using Microsoft.Data.SqlClient;
+using System.ComponentModel.DataAnnotations;
+using TesteBludata.Models.Entity;
+
+namespace TesteBludata.Models.DAO
+{
+    public class EmpresaDAO
+    {
+        SqlConnection con = new SqlConnection("Data Source=DESKTOP-SCS9LTQ;Initial Catalog=TesteBludata;Integrated Security=True;TrustServerCertificate=True;");
+
+        public List<Empresa> ListEmpresa()
+        {
+            var listEmpresa = new List<Empresa>();
+            try
+            {
+                con.Open();
+                string sql = "select * from Empresa";
+                using (SqlCommand cmd = new SqlCommand(sql, con))
+                {
+                    using(var reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            var e = new Empresa();
+                            e.Id = (int)reader["Id"];
+                            e.Nome = reader["Nome"].ToString();
+                            e.UF = reader["UF"].ToString();
+                            e.CPF_ou_CNPJ = reader["CPF_ou_CNPJ"].ToString();
+                            listEmpresa.Add(e);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            finally
+            {
+                con.Close(); 
+            }
+            return listEmpresa;
+        }
+
+        public List<Empresa> EditForIdEmpresa(int id)
+        {
+            string sql = "select * from Empresa where Id=" + id;
+            using (SqlCommand cmd = new SqlCommand(sql, con))
+            {
+                List<Empresa> dados = new List<Empresa>();
+                Empresa e = null;
+                try
+                {
+                    con.Open();
+                    using(var reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            e = new Empresa();
+                            e.Id = (int)reader["Id"];
+                            e.Nome = reader["Nome"].ToString();
+                            e.UF = reader["UF"].ToString();
+                            e.CPF_ou_CNPJ = reader["CPF_ou_CNPJ"].ToString();
+                            dados.Add(e);
+                        }
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception(ex.Message);
+                }
+                finally
+                {
+                   con.Close();
+                }
+                return dados;
+            }
+            
+        }
+
+        public void RegisterEmpresa(Empresa e)
+        {
+            string sql = @"insert into Empresa (Nome, UF, CPF_ou_CNPJ) 
+                        values (@Nome, @UF, @CPF_ou_CNPJ)";
+            SqlCommand cmd = new SqlCommand(sql, con);
+
+            cmd.Parameters.AddWithValue("@Nome",e.Nome);
+            cmd.Parameters.AddWithValue("@UF", e.UF);
+            cmd.Parameters.AddWithValue("@CPF_ou_CNPJ", e.CPF_ou_CNPJ);
+            try
+            {
+                con.Open();
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            finally 
+            {
+                con.Close();
+            }
+        }
+
+        public void EditEmpresa(Empresa e)
+        {
+            string sql = @"update Empresa set Nome=@Nome, UF=@UF,
+                                CPF_ou_CNPJ=@CPF_ou_CNPJ";
+
+            SqlCommand cmd = new SqlCommand(sql, con);
+
+            cmd.Parameters.AddWithValue("@Nome", e.Nome);
+            cmd.Parameters.AddWithValue("@UF", e.UF);
+            cmd.Parameters.AddWithValue("@CPF_ou_CNPJ", e.CPF_ou_CNPJ);
+
+            try
+            {
+                con.Open();
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        public void DeleteEmpresa(int Id)
+        {
+            string sql = @"delete from Empresa where Id=@Id";
+            SqlCommand cmd = new SqlCommand(sql, con);
+            cmd.Parameters.AddWithValue("@Id", Id);
+
+            try
+            {
+                con.Open();
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+    }
+}
